cceb53709e463f0c1d63d588330181492d7f697a
Sureyya-Huseynova <suramail93@gmail.com>
2020-08-31 14:53:51 +0500

    deletes

--- app.js
+++ app.js
@@ -4,12 +4,16 @@ function Add_Card() {
     index++;
     newcard = `
     <div class="card">
+    <span class="delete_card" onclick="DeleteCard()">X</span>
         <div class="head">
             <span class="date"></span>
         </div>
         <hr>
         <div class="card_body" id = "${index}">  
              <div class="mytodo_area">
+                <span class="clear" onclick = "Clear()">
+                <i class="fas fa-trash-alt"  id = "${index}"></i>
+                </span>
                  <div class="mytodo_move" id = "${index}"></div>
                  <input type="text" placeholder="Enter your task" class="mytodo_input"  id = "${index}"  onkeypress="return KeyPressEnter(event);">
                  <span class="save" onclick = "return SaveInputValue(event)">
@@ -27,12 +31,12 @@ function Add_Card() {
     Cards_div.innerHTML += newcard;
     add_inputes = document.querySelectorAll('.add_input');
     add_inputes.forEach(add_input => {
-        add_input.addEventListener("click", showbody)
+        add_input.addEventListener("click", ShowInput)
     });
 };
 
 // bu funksiyada +  click etdikde card da input yaranacaq :)
-function showbody(e) {
+function ShowInput(e) {
     document.querySelectorAll('.card_body').forEach(card_body => {
         if (card_body.id == e.target.id) {
             card_body.style.display = "block";
@@ -40,6 +44,7 @@ function showbody(e) {
     })
 };
 
+// Enter ile save etmek
 function KeyPressEnter(event) {
     if (event.keyCode === 13) {
         SaveInputValue();
@@ -47,18 +52,39 @@ function KeyPressEnter(event) {
     }
 };
 
+var i = 0;
 function SaveInputValue() {
+    i++;
     event_input = event.target.parentElement.parentElement.querySelector('.mytodo_input');    // bura save click etdikde icin doldurduqumuz inputu verir yalniz :) 
     event_mytodo_move = event.target.parentElement.parentElement.querySelector('.mytodo_move'); // bura eyni id beraer olan value kocureceyimiz hisse
     var onetodo = document.createElement('p');
     onetodo.className = "onetask";
+    onetodo.id = i;
     radio = document.createElement('input');
     radio.type = "checkbox";
+    radio.id = i;
     radio.style.marginRight = "1rem";
-    onetodo.appendChild(radio);
-    onetodo.innerHTML += event_input.value;
-    event_mytodo_move.appendChild(onetodo);
-    event_input.value = "";
-         
+    if (event_input.value.length > 0) {
+        onetodo.appendChild(radio);
+        onetodo.innerHTML += event_input.value;
+        event_mytodo_move.appendChild(onetodo);
+        event_input.value = "";
+    }
 };
 
+// isharelediyimiz to do lari silmek
+function Clear() {
+    radioinputes = event.target.parentElement.parentElement.querySelectorAll('input[type=checkbox]');
+    onetodolines = event.target.parentElement.parentElement.querySelectorAll('.onetask');
+    for (j = 0; j < radioinputes.length; j++) {
+        if (radioinputes[j].checked == true) {
+            onetodolines[j].remove();
+            radioinputes[j].remove();
+        }
+    }
+};
+
+// acib istifade etmedyimiz cardi silmek
+function DeleteCard(){
+    event.target.parentElement.remove();
+}
